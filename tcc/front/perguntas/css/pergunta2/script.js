756f887c0f6b9dd1deff2f8b077cbcd96cddb31f
batista29 <natabatista2908@gmail.com>
2023-10-02 16:27:15 -0300

    Novas páginas de perguntas adicionadas

---
+++ tcc/front/perguntas/css/pergunta2/script.js
@@ -0,0 +1,16 @@
+// Selecione todos os botões com a classe 'escolha' e adicione um evento de clique a cada um
+const botoesEscolha = document.querySelectorAll('.escolha');
+botoesEscolha.forEach(botao => {
+    botao.addEventListener('click', function() {
+        // Ao clicar no botão, obtenha o valor do botão clicado
+        const valorBotaoClicado = botao.getAttribute('value');
+        console.log(valorBotaoClicado);
+        // Você pode fazer o que quiser com o valor do botão clicado aqui
+        localStorage.setItem("resp2", valorBotaoClicado);
+    });
+});
+
+// Função para enviar a resposta
+function enviar(valorBotaoClicado) {
+
+}
